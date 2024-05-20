ed98cb87b9445bbb13fca252207f0872f0b7f6dc
danieletulone <danieletulone.work@gmail.com>
2024-05-20 15:59:01 +0200

    fix: removing trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            \App\Http\Middleware\TrustProxies::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
